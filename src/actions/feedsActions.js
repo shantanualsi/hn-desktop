8713347e0b1ef6279839919945f23bfd50714c4e
Shantanu Alshi <shantanu_a@worksap.co.jp>
2018-07-06 18:17:37 +0800

    Fetch HN Top stories details

--- src/actions/feedsActions.js
+++ src/actions/feedsActions.js
@@ -1,17 +1,38 @@
 import * as HNService from "../services/HackernewsService";
 import { INIT_FEED, REFRESH_FEED } from './types'
+import { displayPartsToString } from "typescript";
 
+var endPoint = 'https://hacker-news.firebaseio.com/v0'
+var options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json'
+    }
+}
 /** Dispatch to the reducer */
 export const fetchTopStories = () => (dispatch) => {
     console.log("Fetching Top stories");
-    return HNService.fetchTopStories()
+    
+    fetch(endPoint + '/topstories.json')
+        .then(r => r.json())
         .then(dataArray => {
+            let data = []
+
+            dataArray = dataArray.slice(0, 10);
+            dataArray.forEach(id => {
+                fetch(endPoint + '/item/' + id + '.json', options)
+                    .then(detail => detail.json())
+                    .then(r => data.push(r))
+                    .catch(e => console.log("Something bad happened", e))
+            });
+
             dispatch({
-               type: INIT_FEED,
-               payload: dataArray 
+                type: INIT_FEED,
+                payload: data
             });
         })
         .catch(err => {
             console.log("Error while fetching top stories. ", err);
         });
+
 }
